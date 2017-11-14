735936f73b9822e1fbc1e77f83c5eee02958b8d0
Ryan Padilha <ryan.padilha@gmail.com>
2017-11-14 14:50:54 -0200

    start-sh

--- src/main/resources/bin/start.sh
+++ src/main/resources/bin/start.sh
@@ -12,7 +12,7 @@ APPAWSPATH="/var/wplex/apps/vulcano-health-check-monitor"
 
 echo "starting vulcano-health-check-monitor application..."
 
-java -Xms64m -Xmx256m -Dlogging.config=$APPAWSPATH/config/logback-spring.xml -Dspring.config.location=$APPAWSPATH/config/application-$ENV.properties -jar $APPAWSPATH/lib/vulcano-health-check-monitor.jar &
+java -Xms64m -Xmx256m -Dspring.config.location=$APPAWSPATH/config/application-$ENV.properties -jar $APPAWSPATH/lib/vulcano-health-check-monitor.jar &
 echo $! > /var/run/vulcano-health-check-monitor.pid
 echo "vulcano-health-check-monitor started."
 
